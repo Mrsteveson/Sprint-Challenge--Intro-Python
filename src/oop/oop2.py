537ec864655cb99308b05ab9dd8ec5b9e3dcf01b
mrsteveson <patrick.steveson727@gmail.com>
2019-10-04 10:09:28 -0500

    completed the oop portion, all tests passing for both sections

--- src/oop/oop2.py
+++ src/oop/oop2.py
@@ -4,10 +4,12 @@
 # object is constructed.
 
 class GroundVehicle():
-    def __init__(self, num_wheels):
+    def __init__(self, num_wheels = 4): # Set wheels to 4
         self.num_wheels = num_wheels
 
-    # TODO
+    # TODO # Create a drive method, have it return "vroooom"
+    def drive(self):
+        return "vroooom"
 
 
 # Subclass Motorcycle from GroundVehicle.
@@ -17,7 +19,17 @@ class GroundVehicle():
 #
 # Override the drive() method in Motorcycle so that it returns "BRAAAP!!"
 
-# TODO
+# TODO 
+# Create a Motorcycle subclass
+# Set num_wheels to 2
+# create a new drive method, have it return "BRAAAP!!"
+
+class Motorcycle(GroundVehicle):
+    def __init__(self, num_wheels = 2):
+        self.num_wheels = num_wheels
+
+    def drive(self):
+        return "BRAAAP!!"
 
 vehicles = [
     GroundVehicle(),
@@ -30,3 +42,8 @@ vehicles = [
 # Go through the vehicles list and print the result of calling drive() on each.
 
 # TODO
+# Loop through the list of vehicles
+# Call drive on each as it loops
+
+for i in vehicles:
+    i.drive()
